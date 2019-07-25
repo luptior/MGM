e9a0d2c1b2db4ce2269913a1b6df9fff791dab6e
Gan Xu <luptior@gmail.com>
2019-07-25 00:44:51 -0500

    update

--- src/Server.java
+++ src/Server.java
@@ -55,7 +55,7 @@ public class Server
         }
     }
 
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         Server server = new Server(5000);
     }
